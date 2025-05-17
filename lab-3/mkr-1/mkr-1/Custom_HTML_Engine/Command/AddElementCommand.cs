623a239eaf4f67dcbf8d75aad006bca8428fcd95
EduardKrutii <ipz233_keyu@student.ztu.edu.ua>
2025-05-17 20:01:20 +0200

    Command pattern

---
+++ lab-3/mkr-1/mkr-1/Custom_HTML_Engine/Command/AddElementCommand.cs
@@ -0,0 +1,22 @@
+﻿using Custom_HTML_Engine.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_HTML_Engine.Command
+{
+    public class AddElementCommand : ICommand
+    {
+        private HtmlElement parent, child;
+
+        public AddElementCommand(HtmlElement parent, HtmlElement child)
+        {
+            this.parent = parent;
+            this.child = child;
+        }
+
+        public void Execute() => parent.AddChild(child);
+    }
+}
