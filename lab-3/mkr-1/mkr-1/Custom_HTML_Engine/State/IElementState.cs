b0713ca1bb8fda90723108d90e6217acb32677a2
EduardKrutii <ipz233_keyu@student.ztu.edu.ua>
2025-05-17 20:06:48 +0200

    State pattern

---
+++ lab-3/mkr-1/mkr-1/Custom_HTML_Engine/State/IElementState.cs
@@ -0,0 +1,14 @@
+﻿using Custom_HTML_Engine.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_HTML_Engine.State
+{
+    public interface IElementState
+    {
+        void Handle(HtmlElement element);
+    }
+}
