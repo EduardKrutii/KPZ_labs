dc52cdf934d606757cfff3e609d95826fd6607cc
EduardKrutii <ipz233_keyu@student.ztu.edu.ua>
2025-05-17 19:56:29 +0200

    Модульна контрольна робота 1

---
+++ lab-3/mkr-1/mkr-1/Custom_HTML_Engine/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Custom_HTML_Engine.Command;
+using Custom_HTML_Engine.Elements;
+using Custom_HTML_Engine.Iterator;
+using Custom_HTML_Engine.State;
+using Custom_HTML_Engine.Visitor;
+
+namespace Custom_HTML_Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Div();
+            root.SetText("Root div");
+
+            var p1 = new Paragraph();
+            p1.SetText("Paragraph 1");
+
+            var p2 = new Paragraph();
+            p2.SetText("Paragraph 2");
+
+            ICommand add1 = new AddElementCommand(root, p1);
+            ICommand add2 = new AddElementCommand(root, p2);
+
+            add1.Execute();
+            add2.Execute();
+
+            root.SetState(new LoadingState());
+            root.Render();
+            root.SetState(new RenderedState());
+
+            Console.WriteLine("\nTraversal:");
+            var iterator = new DepthFirstIterator(root);
+            while (iterator.HasNext())
+            {
+                var el = iterator.Next();
+                Console.WriteLine($"Element: {el.GetType().Name}");
+            }
+
+            Console.WriteLine("\nVisitor:");
+            var visitor = new CountVisitor();
+            root.Accept(visitor);
+            foreach (var child in root.GetChildren())
+                child.Accept(visitor);
+
+            Console.WriteLine($"Divs: {visitor.DivCount}, Paragraphs: {visitor.ParagraphCount}");
+        }
+    }
+}
