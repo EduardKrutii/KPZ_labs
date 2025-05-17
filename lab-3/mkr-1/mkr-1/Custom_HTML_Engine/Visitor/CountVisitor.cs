25d2dd9f818bb9b471547486e6bb889283b002f0
EduardKrutii <ipz233_keyu@student.ztu.edu.ua>
2025-05-17 20:07:50 +0200

    Visitor pattern

---
+++ lab-3/mkr-1/mkr-1/Custom_HTML_Engine/Visitor/CountVisitor.cs
@@ -0,0 +1,18 @@
+﻿using Custom_HTML_Engine.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_HTML_Engine.Visitor
+{
+    public class CountVisitor : IHtmlVisitor
+    {
+        public int DivCount = 0;
+        public int ParagraphCount = 0;
+
+        public void VisitDiv(Div div) => DivCount++;
+        public void VisitParagraph(Paragraph p) => ParagraphCount++;
+    }
+}
