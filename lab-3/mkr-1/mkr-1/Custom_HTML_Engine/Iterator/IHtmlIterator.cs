827ae7cfac15fd03d67786d27490192a033dea20
EduardKrutii <ipz233_keyu@student.ztu.edu.ua>
2025-05-17 20:05:11 +0200

    Iterator pattern

---
+++ lab-3/mkr-1/mkr-1/Custom_HTML_Engine/Iterator/IHtmlIterator.cs
@@ -0,0 +1,15 @@
+﻿using Custom_HTML_Engine.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_HTML_Engine.Iterator
+{
+    public interface IHtmlIterator
+    {
+        bool HasNext();
+        HtmlElement Next();
+    }
+}
